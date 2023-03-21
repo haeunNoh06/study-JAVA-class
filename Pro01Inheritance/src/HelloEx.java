0f3f94483502f790999b82e12c5b63fe5dd0264a
haeunNoh06 <okhagrace06@naver.com>
2023-03-21 13:21:32 +0900

    기본 자바 출력

---
+++ Pro01Inheritance/src/HelloEx.java
@@ -0,0 +1,13 @@
+
+public class HelloEx {
+
+	public static void main(String[] args) {
+
+		int a = 123, b;
+		double x = 345.678, y;
+		
+		y = a;
+		b = (int)x;
+	}
+
+}
--- Pro01Inheritance/src/in01/Point.java
+++ Pro01Inheritance/src/in01/Point.java
@@ -4,7 +4,8 @@ public class Point {
 	
 	private int x, y; // 한 점을 구성하는 x, y 좌표
 	
-	public void set(int x, int y) {
+	public void set(int x, int y) { // x,y는 스택 영역에 저장되고
+		// 메서드 실행이 종료되면 x, y 변수는 사라진다.
 		this.x = x;
 		this.y = y;
 	}
